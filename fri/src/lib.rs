cdb3585004ce574446acdc00438be4d226299322
Son Nguyen Thanh <thanhsondeptrai243@gmail.com>
2024-03-30 22:09:34 +0700

    feat: fold polynomial

--- fri/src/lib.rs
+++ fri/src/lib.rs
@@ -1,3 +1,15 @@
-mod hasher;
-mod merkle_tree;
-mod fields;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
