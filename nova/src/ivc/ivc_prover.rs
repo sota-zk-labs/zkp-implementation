fece4f6e36708a13dad06e61c7232addaaf3ee96
vanhger <vietanhpg2003@gmail.com>
2024-05-14 23:07:49 +0700

    Rewrite the simple test case into a step-by-step IVC test case

--- nova/src/ivc/ivc_prover.rs
+++ nova/src/ivc/ivc_prover.rs
@@ -4,7 +4,7 @@ use kzg::types::{ScalarField};
 use crate::circuit::{FCircuit};
 use crate::ivc::{IVC, IVCProof, ZkIVCProof};
 use crate::nifs::NIFS;
-use crate::r1cs::R1CS;
+use crate::r1cs::{FInstance, FWitness, R1CS};
 use crate::transcript::Transcript;
 
 impl <T: Digest + Default + ark_serialize::Write, FC: FCircuit > IVC <T, FC> {
@@ -13,7 +13,7 @@ impl <T: Digest + Default + ark_serialize::Write, FC: FCircuit > IVC <T, FC> {
         r1cs: &R1CS<ScalarField>,
         ivc_proof: &IVCProof,
         prover_transcript: &mut Transcript<T>,
-    ) -> ZkIVCProof {
+    ) -> (FWitness, FInstance, ZkIVCProof) {
 
         let i = self.augmented_circuit.i;
         if ! i.is_zero() {
@@ -29,19 +29,27 @@ impl <T: Digest + Default + ark_serialize::Write, FC: FCircuit > IVC <T, FC> {
 
             let nifs_proof = NIFS::<T>::prove(r, &big_w_out, &big_u_out, &self.scheme, prover_transcript);
 
-            ZkIVCProof {
-                u_i: ivc_proof.u_i.clone(),
-                big_u_i: ivc_proof.big_u_i.clone(),
-                com_t: Some(com_t),
-                folded_u_proof: Some(nifs_proof)
-            }
+            (
+                big_w_out,
+                big_u_out,
+                ZkIVCProof {
+                    u_i: ivc_proof.u_i.clone(),
+                    big_u_i: ivc_proof.big_u_i.clone(),
+                    com_t: Some(com_t),
+                    folded_u_proof: Some(nifs_proof)
+                })
+
         } else {
-            ZkIVCProof {
-                u_i: ivc_proof.u_i.clone(),
-                big_u_i: ivc_proof.big_u_i.clone(),
-                com_t: None,
-                folded_u_proof: None
-            }
+            (
+                ivc_proof.big_w_i.clone(),
+                ivc_proof.big_u_i.clone(),
+                ZkIVCProof {
+                    u_i: ivc_proof.u_i.clone(),
+                    big_u_i: ivc_proof.big_u_i.clone(),
+                    com_t: None,
+                    folded_u_proof: None
+                })
+
         }
     }
 }
